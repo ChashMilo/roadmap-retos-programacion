17ab400b4fac9eded41bf61bec9fec7e5097f960
ChashMilo <154559733+ChashMilo@users.noreply.github.com>
2025-12-26 02:53:16 -0500

    CashMilo.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CashMilo.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CashMilo.py
@@ -1,1 +1,58 @@
+# https://www.python.org/
 
+#Comentario a una linea 
+
+"""
+Esto es 
+en teoria...
+un comentario a varias lineas
+"""
+
+'''
+Esto tambien es
+otra forma de multilenia en python
+'''
+
+#Variable en Python
+
+"""
+El "snake_case" en Python es una convención
+de nomenclatura donde las palabras compuestas
+se unen usando guiones bajos _, y todas las
+letras son minúsculas (ej: nombre_de_usuario).
+
+Literalmente solo es que en vez de espacio se usa un guion bajo,
+es_decir_se_escribiria_asi (es para las variables unicamente)
+"""
+# Tipo de variable tipo cadena de texto = string.
+"""Cuando asignamos un valor entre comillas (simples o dobles), la variable
+automáticamente toma el tipo `str` (cadena de texto). Incluso si parece un número.
+"""
+#(esto lo copie de otro documento :p
+ 
+my_variable = "mi variable"
+
+"""
+una constante es un identificador que representa un valor
+fijo que no puede ser modificado durante la ejecución de un programa
+
+Python no tiene costantes, todos los datos son viariables.
+"""
+#Por convención se pone en MAYUSCULA para identificar que no se cambie
+
+MY_CONSTANT = "Es una constante que no es constante" #por convención
+
+"""DATOS PRIMITIVOS"""
+#los numero ya sea entero (Int) o decimal (Float), solo se le asignan y ya, no hay constantes
+my_int = 1 
+my_float = 1.5
+
+#En los Bool solo es asignarle el valor True o False (las mayusuclas incluidas
+my_bool = True
+my_boll = False
+
+#Las cadenas de texto String o Str, se pone con comillas comilla simple o doble
+my_str = "Hola soy una "constante" de texto"
+
+#Para imprimir texto por consola solo es poner print()
+print("¡Hola, Python!")
